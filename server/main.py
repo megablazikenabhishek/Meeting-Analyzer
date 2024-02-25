a8978cc057e86a4060234e9b640dffaf02f48868
Abhishek Upadhyay <96608611+megablazikenabhishek@users.noreply.github.com>
2024-02-25 15:44:55 +0000

    fs

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
 from flask import Flask, request
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
+# for summarization
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 tokenizer = AutoTokenizer.from_pretrained("knkarthick/MEETING_SUMMARY", model_max_length=5000)
 model = AutoModelForSeq2SeqLM.from_pretrained("knkarthick/MEETING_SUMMARY")
+
+# importing required files
+from utils.cleanData import cleanData
 
 # intializing app
 app = Flask(__name__)
@@ -11,16 +15,32 @@ app = Flask(__name__)
 def home():
 	return {'sucess': True, 'message': 'Hello World!!'}
 
+@app.route("/cleanData", methods=['GET', 'POST'])
+def clean_data():
+	try:
+		req = request.get_json()
+		data = req['data']
+
+		# cleaning data
+		result = cleanData(data)
+		print(result)
+		return {'sucess': True, 'data': result}
+	except Exception as e:
+		return {'sucess': False, 'message': str(e)}
+
 @app.route("/generateSummary", methods=['GET', 'POST'])
 def summary():
-	req = request.get_json()
-	text = req['text']
-	
-	inputs = tokenizer(text, return_tensors="pt", max_length=5000, truncation=True)
-	summary_ids = model.generate(inputs["input_ids"], max_length=500, min_length=250, length_penalty=0.5, num_beams=4, early_stopping=False)
-	summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+	try:
+		req = request.get_json()
+		text = req['text']
+		
+		inputs = tokenizer(text, return_tensors="pt", max_length=5000, truncation=True)
+		summary_ids = model.generate(inputs["input_ids"], max_length=500, min_length=250, length_penalty=0.5, num_beams=4, early_stopping=False)
+		summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
-	return {'sucess': True, 'summary': summary}
+		return {'sucess': True, 'summary': summary}
+	except Exception as e:
+		return {'sucess': False, 'message': str(e)}
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=5000)
---
+++ server/utils/cleanData.py
@@ -0,0 +1,46 @@
+# for similarity check
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def getSimilarity(text1, text2):
+    vectorizer = TfidfVectorizer()
+    vectorized_text1 = vectorizer.fit_transform([text1])
+    vectorized_text2 = vectorizer.transform([text2])
+
+    # Calculate cosine similarity
+    cosine_sim = cosine_similarity(vectorized_text1, vectorized_text2)[0][0]
+
+    # Convert similarity to percentage
+    similarity_percentage = (cosine_sim + 1) * 50
+
+    return similarity_percentage
+
+def cleanData(data):
+    result = []
+
+    # sorting data based on length
+    # data.sort(key=lambda x: len(x['text']), reverse=False)
+
+    # print(len(data))
+
+    for i in range(0, len(data)):
+        for j in range(i+1, len(data)):
+            if data[i]['name'] != data[j]['name']:  
+                continue
+
+            text1 = data[i]['text']
+            text2 = data[j]['text']
+
+            # getting similarity
+            similarity = getSimilarity(text1, text2[:len(text1)])
+
+            # print(f"{text1} ||| {text2} --> {similarity}")
+            if similarity > 80:
+                data[i]['text'] = text2
+    
+    # adding only unique data to results
+    for i in data:
+        if i not in result:
+            result.append(i)
+
+    return result
